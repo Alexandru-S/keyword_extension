e0103dd26d183877a53b5f8d3ae1afb4844fe5b6
Alexandru-S <suleaa@tcd.ie>
2018-04-23 00:46:13 +0100

    changed bckground to any other color

--- js/popup.js
+++ js/popup.js
@@ -1,3 +1,19 @@
 /**
  * Created by owner on 22/04/2018.
  */
+var changeColor = document.getElementById('changeColor');
+
+chrome.storage.sync.get('color', function(data) {
+    changeColor.style.backgroundColor = data.color;
+    changeColor.setAttribute('value', data.color);
+});
+
+changeColor.onclick = function(element) {
+    var color = element.target.value;
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            {code: 'document.body.style.backgroundColor = "' + color + '";'});
+    });
+};
+
